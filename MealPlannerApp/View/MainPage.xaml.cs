73dd42bfb95a781c469707697cd8f90ebd0b1815
Christian <christian.sanchez.co@gmail.com>
2023-09-25 11:15:55 +0200

    Added MVVM structure and used dependency injector to pass the view models. Changed the main page and added new icon

---
+++ MealPlannerApp/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MealPlannerApp.ViewModel;
+
+namespace MealPlannerApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+
+        //BindingContext = new MainViewModel();        
+        BindingContext = vm;
+    }
+}
+
---
+++ MealPlannerApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MealPlannerApp.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string myTaskText;
+
+    [RelayCommand]
+    async void Add()
+    {
+        if (string.IsNullOrEmpty(MyTaskText))
+        {
+            return;
+        }
+
+
+        Items.Add(MyTaskText);
+        MyTaskText = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string stringToDelete)
+    {
+        if (stringToDelete != null && Items.Contains(stringToDelete))
+        {
+            Items.Remove(stringToDelete);
+        }
+    }
+
+    /// <summary>
+    /// Asynchronous function for handling a tap event with a parameter.
+    /// When called, it navigates to the 'DetailPage' using Shell navigation and includes a query parameter 'Text' with the provided 'stringToPass' value.
+    /// The 'stringToPass' parameter is used to pass additional information to the 'DetailPage'.
+    /// </summary>
+    /// <param name="stringToPass"></param>
+    /// <returns></returns>
+    [RelayCommand]
+    async Task Tap(string stringToPass)
+    {
+        //await Shell.Current.GoToAsync($"{nameof(DetailPage)}?TextToPass={stringToPass}");
+
+        //Example passing complex data types
+        //await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={stringToPass}",
+        //    new Dictionary<string, object>
+        //    {
+        //        {nameof(DetailPage),new object()},
+        //    });
+
+    }
+
+
+}
