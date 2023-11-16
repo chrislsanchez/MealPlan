770db31bd2b170ba3327df9a67063786b5224dfc
Christian <christian.sanchez.co@gmail.com>
2023-11-16 14:20:26 +0100

    Deleted .net 6 solution and cleaned up

--- MealPlannerApp/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Input;
-global using MealPlannerApp.Model;
-global using MealPlannerApp.ViewModel;
-global using System.Collections.ObjectModel;
-global using SQLite;
